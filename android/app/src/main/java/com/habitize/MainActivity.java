3b0c41782079b5aa1f36b53dbacf7a91a15f91ed
kashan16 <mohdkashan21165@gmail.com>
2025-07-29 19:46:46 +0530

    Build Mobile App using capacitor

---
+++ android/app/src/main/java/com/habitize/MainActivity.java
@@ -0,0 +1,5 @@
+package com.habitize;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.habitize',
+  appName: 'Habitize',
+  webDir: 'out'
+};
+
+export default config;
