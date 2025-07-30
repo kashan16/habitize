06cf6293c3f339588147eca13e9df53158b01fa3
kashan16 <mohdkashan21165@gmail.com>
2025-07-30 11:38:35 +0530

    Fixed A

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.habitize',
   appName: 'Habitize',
-  webDir: 'out'
+  webDir: 'out',
+  server : {
+    androidScheme : 'https'
+  }
 };
 
 export default config;
